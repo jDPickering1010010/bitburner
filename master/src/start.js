05d34fef12db7f4ac4645da23a080724780987f0
[[_Joshua-Pickering_]] <117299001+jDPickering1010010@users.noreply.github.com>
2025-06-11 19:31:18 -0500

    Update start.js

--- master/src/start.js
+++ master/src/start.js
@@ -3,7 +3,7 @@ export async function main(ns) {
     throw new Exception("Run the script from home");
   }
 
-  await ns.wget(https://github.com/jDPickering1010010/bitburner//master/src/initHacking.js?ts=${new Date().getTime()}`,
+  await ns.wget(https://github.com/jDPickering1010010/bitburner/master/src/initHacking.js?ts=${new Date().getTime()}`,
     "initHacking.js"
   );
   ns.spawn("initHacking.js", 1);
