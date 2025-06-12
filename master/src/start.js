14af998ea66721e049f496f28b549cf76f6f7684
[[_Joshua-Pickering_]] <117299001+jDPickering1010010@users.noreply.github.com>
2025-06-11 19:27:41 -0500

    Create start.js

---
+++ master/src/start.js
@@ -0,0 +1,10 @@
+export async function main(ns) {
+  if (ns.getHostname() !== "home") {
+    throw new Exception("Run the script from home");
+  }
+
+  await ns.wget(https://github.com/jDPickering1010010/bitburner//master/src/initHacking.js?ts=${new Date().getTime()}`,
+    "initHacking.js"
+  );
+  ns.spawn("initHacking.js", 1);
+}
